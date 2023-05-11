221379f4b1757907b3248c483b789a2a104992d7
CORP\ikommh <kobymiz@towersemi.com>
2023-05-11 15:59:56 +0300

    Start image galleryimplementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { EventsComponent } from './events/events.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LOCALE_ID } from '@angular/core';
@@ -15,6 +15,7 @@ registerLocaleData(localeHe);
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookingModule } from './booking/booking.module';
+import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
 
 @NgModule({
   declarations: [
@@ -30,9 +31,11 @@ import { BookingModule } from './booking/booking.module';
     AppRoutingModule,
     BookingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Angular2ImageGalleryModule,
+    HammerModule
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'he-IL'}],
+  providers: [{ provide: LOCALE_ID, useValue: 'he-IL' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
